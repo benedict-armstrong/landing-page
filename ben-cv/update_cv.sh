afd4add4db8a298f0d71bc1e67139a8b82b21129
Benedict Armstrong <contact@benarmstro.ng>
2022-10-22 18:47:24 +0200

    deleted old landing page

--- ben-cv/update_cv.sh
+++ ben-cv/update_cv.sh
@@ -1,2 +1,5 @@
-scp CV_documents/CV-de.pdf ben-docker-1:benarmstro.ng/cv/
-cp  CV_documents/CV-de.pdf html/CV-de.pdf
+scp CV_documents/CV-en.pdf ben-docker-1:benarmstro.ng/
+ssh ben-docker-1 "cd benarmstro.ng/ && sudo mv CV-en.pdf cv/"
+cp CV_documents/CV-en.pdf html/CV-en.pdf
+# scp CV_documents/CV-de.pdf ben-docker-1:benarmstro.ng/
+# cp CV_documents/CV-de.pdf html/CV-de.pdf
--- landing-page/deploy.sh
+++
@@ -1,4 +0,0 @@
-npm run build
-docker build --platform linux/amd64 . -t benarmstrong/landing-page:latest
-docker push benarmstrong/landing-page:latest
-ssh ben-docker-1 'cd benarmstro.ng; docker-compose pull landing-page; docker-compose up -d'
