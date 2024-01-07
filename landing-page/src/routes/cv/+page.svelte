52c609c5bdfd8376e59d1d237aadfb90a98e5ff4
Benedict Armstrong <contact@benarmstro.ng>
2024-01-07 13:00:34 +0100

    added cv

---
+++ landing-page/src/routes/cv/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	// Redirect to files/cv.pdf
+	window.location.replace('files/cv.pdf');
+</script>
+
+<h1 class="text-3xl font-bold mb-3">CV</h1>
+<!-- Download link -->
+<div class="text-2xl font-bold mt-3 flex flex-wrap">
+	<a
+		href="files/cv.pdf"
+		download="cv.pdf"
+		class="bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent hover:underline"
+		>Download PDF</a
+	>
+</div>
