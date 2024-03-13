b687a96ba8f2ef305e4ba27d4047fca3a3884740
Benedict Armstrong <contact@benarmstro.ng>
2024-03-13 11:34:55 +0100

    fixed CV link

--- landing-page/src/routes/cv/+page.svelte
+++ landing-page/src/routes/cv/+page.svelte
@@ -1,13 +1,17 @@
 <script>
-	// Redirect to files/cv.pdf
-	window.location.replace('files/cv.pdf');
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		// Redirect to files/cv.pdf
+		window.location.replace('/files/cv.pdf');
+	}
 </script>
 
 <h1 class="text-3xl font-bold mb-3">CV</h1>
 <!-- Download link -->
 <div class="text-2xl font-bold mt-3 flex flex-wrap">
 	<a
-		href="files/cv.pdf"
+		href="/files/cv.pdf"
 		download="cv.pdf"
 		class="bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent hover:underline"
 		>Download PDF</a
